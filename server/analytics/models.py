5f742aa122be691943fc254b2f8cbc539e8e7495
FUTURE_FS_01 <shivani82208@gmail.com>
2026-02-07 11:43:48 +0530

    Added EquipmentAnalysis model for history

--- server/analytics/models.py
+++ server/analytics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class EquipmentAnalysis(models.Model):
+    filename = models.CharField(max_length=255)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    total_items = models.IntegerField()
+    average_risk = models.FloatField()
+
+    def __str__(self):
+        return f"{self.filename} ({self.upload_date.strftime('%Y-%m-%d')})"
+
+    class Meta:
+        ordering = ['-upload_date']
